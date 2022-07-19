98b9c792ce754a158c42679503b5e5551e89805d
Iturria <Maximiliano.Iturria@sbdinc.com>
2022-07-19 15:42:07 +0200

    each product saved in its cat folder (opt fields leeg)

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from pickletools import optimize
 import openpyxl
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Font
@@ -6,6 +7,7 @@ from datetime import datetime
 import os.path
 import os
 
+column_widths = []
 print("Loading files")
 allsku = openpyxl.load_workbook('All_SKUs_21.06.xlsx')
 allsku = allsku[allsku.sheetnames[0]]
@@ -17,7 +19,7 @@ glossary.delete_rows(1, 4)
 print("Loaded glossary")
 
 
-headers = [cell.value for cell in allsku[1]] # all product column names
+headers = [cell.value for cell in allsku[1]]  # all product column names
 
 print(headers)
 #           category name   mandatory
@@ -32,15 +34,15 @@ for row in glossary:
         categories[categoryid].extend([(row[5].value, row[2].value)])
 
 for i, row in enumerate(allsku):
-    if i == 0: # skip headers
+    if i == 0:  # skip headers
         continue
+    gs1 = str(row[0].value)
     categoryid = str(row[4].value)
     if categoryid not in categories.keys():
-        categoryid = '10000334' # UNKNOWN CAT ALL TREATED AS : Bath/shower accessory type
+        categoryid = '10000334'  # UNKNOWN CAT ALL TREATED AS : Bath/shower accessory type
 
     if not os.path.exists(categoryid):
         os.mkdir(categoryid)
-
 
     clean_wb = Workbook()
     clean_ws = clean_wb.active
@@ -67,5 +69,35 @@ for i, row in enumerate(allsku):
     print(len(req))
     print(len(req_vals))
     print()
+    clean_wb = Workbook()
+    clean_ws = clean_wb.active
+    clean_ws.append(["GS1 item number (GTIN):", gs1, "BRICK CAT:", categoryid, "v0.1",
+                     "Script last executed:", datetime.today().strftime('%Y-%m-%d %H:%M:%S')])
+    clean_ws.append(req)
+    clean_ws.append(req_vals)
+    clean_ws.append(opt)
+    clean_ws.append(opt_vals)
+    for cell in clean_ws['A2:'+chr(len(req_vals)+65)+'2'][0]:
+        cell.fill = PatternFill("solid", start_color="ed5587")
+        cell.font = Font(bold=True)
+    for cell in clean_ws['A4:BR4'][0]:
+        cell.fill = PatternFill("solid", start_color="97ed55")
+        cell.font = Font(italic=True)
+    for row in clean_ws:
+        for i, cell in enumerate(row):
+            if len(column_widths) > i:
+                if len(str(cell)) > column_widths[i]:
+                    column_widths[i] = len(cell)
+            else:
+                column_widths += [len(str(cell))]
+    for i, column_width in enumerate(column_widths, 1):
+        clean_ws.column_dimensions[get_column_letter(
+            i)].width = column_width
+    clean_wb.save(str(categoryid)+"\\"+str(gs1)+".xlsx")
+    req = list()
+    req_vals = list()
+    opt = list()
+    opt_vals = list()
+    clean_wb.close()
     # print(opt)
     # print(opt_vals)
